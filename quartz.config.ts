a6bca677240c6dc99758532f85367f72cf6392d9
proxylivy <zunigagabriel3@gmail.com>
2025-02-20 19:48:11 -0300

    Quartz sync: Feb 20, 2025, 7:48 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Proxylivy Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "es-CL",
+    baseUrl: "notes.proxylivy.work",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,

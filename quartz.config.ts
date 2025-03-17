15281322e750cc78811f438e59ad71c674894c2e
proxylivy <zunigagabriel3@gmail.com>
2025-03-17 13:09:40 -0300

    Quartz sync: Mar 17, 2025, 1:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "es-ES",
     baseUrl: "notes.proxylivy.work",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "000 - Config/Plantillas", "999 - Archivado"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
